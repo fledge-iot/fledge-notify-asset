4643984faa20b9831c97c2957a25019d2f51d654
Innovative-ashwin <sinha.ashwini111@gmail.com>
2023-03-24 18:48:34 +0530

    fixed code review comments
    Signed-off-by: Innovative-ashwin <sinha.ashwini111@gmail.com>

--- asset.cpp
+++ asset.cpp
@@ -41,12 +41,14 @@ Asset::~Asset()
  * @param notificationName 	The name of this notification
  * @param triggerReason		Why the notification is being sent
  * @param message		The message to send
+ * @return bool			whether notify succeded or false
  */
 bool Asset::notify(const string& notificationName, const string& triggerReason, const string& message)
 {
 	vector<Datapoint *>	datapoints;
 	if (!m_ingest)
 	{
+		Logger::getLogger()->error("m_ingest is null");
 		return false;
 	}
 
